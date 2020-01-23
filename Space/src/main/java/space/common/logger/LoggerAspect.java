245d4b80a18647e4f8bf952aae50f1dac091e62d
한얼 <qdsa09@gmail.com>
2020-01-23 13:56:21 +0900

    프로젝트 기본 세팅입니다

---
+++ Space/src/main/java/space/common/logger/LoggerAspect.java
@@ -0,0 +1,28 @@
+package space.common.logger;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+
+public class LoggerAspect {
+	protected Log log=LogFactory.getLog(this.getClass());
+	static String name="";
+	static String type="";
+	
+	@Around("execution( * first..controller.*Controller.*(..)) or execution(*first..service.*Impl.*(..))"
+			+ " or execution(* first..dao.*DAO.*(..))")
+	public Object logPrint(ProceedingJoinPoint joinPoint) throws Throwable {
+		type=joinPoint.getSignature().getDeclaringTypeName();
+		
+		if(type.indexOf("Controller") > -1) {
+			name = "Controller \t:  ";
+		}else if(type.indexOf("Service") > -1 ) {
+			name = "Service  \t  :";
+		}else if(type.indexOf("DAO") > -1) {
+			name = "DAO  \t  :";
+		}
+		log.debug(name + type + "."+joinPoint.getSignature().getName() + "()");
+		return joinPoint.proceed();
+	}
+}
