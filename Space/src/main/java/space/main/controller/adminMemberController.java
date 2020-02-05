227c4eddfbc4c35c049ee4a4f621f99777ef0a65
세범 <osb0427@naver.com>
2020-02-05 14:42:10 +0900

    관리자목록 수정

--- Space/src/main/java/space/main/controller/adminMemberController.java
+++ Space/src/main/java/space/main/controller/adminMemberController.java
@@ -1,22 +1,33 @@
 package space.main.controller;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
 
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import space.admin.service.AdminMemberService;
 import space.common.common.CommandMap;
+
 
 @Controller
 public class adminMemberController {
-	
-	@RequestMapping("admin/memberList")
-	public ModelAndView AdminMemberList(CommandMap commandMap, 
-			HttpServletRequest request) throws Exception { 
-		ModelAndView mv = new ModelAndView("admin/memberList");
-		return mv;
-	}
-	
+	Logger log = Logger.getLogger(this.getClass());
 
+	@Resource(name="AdminMemberService")
+	private AdminMemberService adminMemberService;
+	
+	@RequestMapping(value="/admin/memberList")
+    public ModelAndView memberList(CommandMap commandMap) throws Exception{
+    	ModelAndView mv = new ModelAndView("/admin/memberList");
+    	
+    	List<Map<String,Object>> list = adminMemberService.selectMemberList(commandMap.getMap());
+    	mv.addObject("list", list);
+    	
+    	return mv;
+    }
 }
